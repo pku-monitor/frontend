e84d244808230ac4527f0c3c05c8a7a3510adf3e
EricAKPM <ericmili.m7@gmail.com>
2021-06-04 15:00:57 -0400

    Redux, saga, store da tela cadastrar produto

--- src/store/modules/rootReducer.js
+++ src/store/modules/rootReducer.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
 import home from '../modules/Home/reducer.js'
 import login from '../modules/Login/reducer'
+import register from '../modules/Registro/reducer'
 
-export const rootReducer = combineReducers({home, login})
+export const rootReducer = combineReducers({home, login, register})
 
 
